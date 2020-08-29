a135c1cf22dfb630e34c3f9136959d571639731d
Gene <Genejob@163.com>
2020-08-30 00:01:23 +0800

    - vue cli创建的项目中修改 项目入口文件

--- vue-doc/vue.config.js
+++ vue-doc/vue.config.js
@@ -1,4 +1,13 @@
+/**
+ * 在 vue 中配置 webpack
+ */
 module.exports = {
+  // 修改项目的入口文件, 可以修改 configureWebpack 对象的 entry 属性
+  // 可以修改 entry 这个属性来查看对应不同的知识总结
+  configureWebpack: {
+    entry: './src/main.js',  // 默认入口 ./src/main.js
+    // entry: './01.src-插槽/main.js', // 修改入口 ./01.src-插槽/main.js
+  },
   devServer: {
     overlay: {
       warnings: true,
