f653a538367b6ef40eaf266ebb75e76da5eca14b
Gene <Genejob@163.com>
2020-08-30 00:03:13 +0800

    - vue 项目中配置 webpack

--- vue-doc/vue.config.js
+++ vue-doc/vue.config.js
@@ -5,8 +5,8 @@ module.exports = {
   // 修改项目的入口文件, 可以修改 configureWebpack 对象的 entry 属性
   // 可以修改 entry 这个属性来查看对应不同的知识总结
   configureWebpack: {
-    entry: './src/main.js',  // 默认入口 ./src/main.js
-    // entry: './01.src-插槽/main.js', // 修改入口 ./01.src-插槽/main.js
+    // entry: './src/main.js',  // 默认入口 ./src/main.js
+    entry: './01.src-插槽/main.js', // 修改入口 ./01.src-插槽/main.js
   },
   devServer: {
     overlay: {
