368e69ddca0752d2baa8c60ee2e2bca1dcbd6262
Gene <Genejob@163.com>
2020-09-03 10:39:07 +0800

    - 作用域插槽

---
+++ vue-doc/02.src-动态组件,异步组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        Hello,Vue 动态组件, 异步组件
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+    components: {}
+  }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
