fb8240030aa24ccf3bdb73c648b4ca95235bf719
Gene <Genejob@163.com>
2020-11-17 08:30:46 +0800

    vue 笔记

--- vue-doc/src/App.vue
+++ vue-doc/src/App.vue
@@ -1,24 +1,19 @@
 <template>
-    <div id="app">
-        Hello,Vue
-    </div>
+  <div id="app">
+    <h4 style="color: deeppink"> Hello Vue!</h4>
+
+  </div>
 </template>
 
 <script>
 
-  export default {
-    name: 'App',
-    components: {}
-  }
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+}
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
 </style>
